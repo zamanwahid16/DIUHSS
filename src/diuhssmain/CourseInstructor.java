4f6567cfe56af2f94e079bf314c0c2630fa3c33c
Zaman Wahid <zamanwahid16@gmail.com>
2020-05-06 20:59:04 +0600

    upload

--- src/diuhssmain/CourseInstructor.java
+++ src/diuhssmain/CourseInstructor.java
@@ -11,7 +11,7 @@ import java.io.PrintWriter;
 
 /**
  *
- * @author Shahin
+ * @author Zaman
  */
 public class CourseInstructor extends Information{
     private String employeeID,designation, greScore,ieltsToeflScore,reference;
